4dbc026e92116727119e87f4573509d6cf3db9e1
Aniruddha Ghosh <aghosh0605@gmail.com>
2023-05-20 15:14:13 +0530

    🔧 Celery & DynamoDB Insert Implemented

--- video_sub_parser/apis/extractor.py
+++ video_sub_parser/apis/extractor.py
@@ -9,22 +9,6 @@ import json
 
 _dbobj = DynamoServices()
 
-def uploadSubtitle(subtitle_path,media_url):
-    s3_subtitle_url= os.path.join(settings.BUCKET_URL,f'subtitles/{os.path.basename(subtitle_path)}')
-    s3_upload_url =os.path.join(settings.BUCKET_URL,f'uploads/{os.path.basename(media_url)}')
-    data_to_upload = []
-    
-    with open(subtitle_path) as f:
-        for line in f:
-            words = line.split('|')
-            # print(words)
-            data={"SubtitleID": str(uuid.uuid4()),"start_time":words[0].strip(), "end_time":words[1].strip(),"subtitle":words[3].strip(), "media_url":s3_upload_url, "subtitle_url":s3_subtitle_url}
-            data_to_upload.append(data)
-            # print(json.dumps(data, indent=4))
-            
-        print(f'\n=======>Total count of subtitles: {len(data_to_upload)}')
-        # _dbobj.createItem(data_to_upload)
-
 def extractSubtitle(video_path):
     filename = uuid.uuid1()
     subtitle_path = os.path.join(settings.BASE_DIR,f'media/subtitles/{filename}.txt')
@@ -38,8 +22,6 @@ def extractSubtitle(video_path):
     # print(returned_output.decode("utf-8"))
     
     if os.path.isfile(subtitle_path):
-        sleep_sometime.delay()
         # upload_file(subtitle_path)
-        # uploadSubtitle(subtitle_path,video_path)
+        puItems.delay(subtitle_path,video_path)
         # os.remove(video_path)
-        os.remove(subtitle_path)
--- video_sub_parser/apis/tasks.py
+++ video_sub_parser/apis/tasks.py
@@ -1,8 +1,29 @@
-
 from celery import shared_task
 import time
+import os
+from .dynamodb import DynamoServices
+from django.conf import settings
+import uuid
+import json
+
+_dbobj=DynamoServices()
 
 @shared_task
-def sleep_sometime():
-    time.sleep(20)
-    print('Recharge Done')
+def puItems(subtitle_path,media_url):
+    
+    s3_subtitle_url= os.path.join(settings.BUCKET_URL,f'subtitles/{os.path.basename(subtitle_path)}')
+    s3_upload_url =os.path.join(settings.BUCKET_URL,f'uploads/{os.path.basename(media_url)}')
+    data_to_upload = []
+    
+    with open(subtitle_path) as f:
+        for line in f:
+            words = line.split('|')
+            # print(words)
+            data={"SubtitleID": str(uuid.uuid4()),"start_time":words[0].strip(), "end_time":words[1].strip(),"subtitle":words[3].strip(), "media_url":s3_upload_url, "subtitle_url":s3_subtitle_url}
+            data_to_upload.append(data)
+            # print(json.dumps(data, indent=4))
+            
+        print(f'\n=======>Total count of subtitles: {len(data_to_upload)}')
+    _dbobj.createItem(data_to_upload)
+    os.remove(subtitle_path)
+    print('Insert background task completed')
