b2d904f9fc0a785dc6c4692bdd2c607da56de272
Aniruddha Ghosh <aghosh0605@gmail.com>
2023-05-22 16:07:25 +0530

    🚧 Tailwinds & MUI Initiated

--- client/src/components/form.jsx
+++ client/src/components/form.jsx
@@ -1,29 +1,37 @@
 import React, { useState } from "react";
+import CircularIndeterminate from "./uploading";
+import Button from "@mui/material/Button";
 
 const BASE_URL = "http://127.0.0.1:8000";
 
 export const FileUpload = () => {
   const [selectedFile, setSelectedFile] = useState();
   const [isFilePicked, setIsFilePicked] = useState(false);
+  const [isFileSent, setIsFileSent] = useState(false);
 
   const changeHandler = (event) => {
     setSelectedFile(event.target.files[0]);
     setIsFilePicked(true);
   };
 
-  const handleSubmission = () => {
+  const handleSubmission = async () => {
     const formData = new FormData();
 
     formData.append("file", selectedFile);
     formData.append("remark", "File to parse");
 
-    fetch(`${BASE_URL}/api/v1/upload/file`, {
+    await fetch(`${BASE_URL}/api/v1/upload/file`, {
       method: "POST",
       body: formData,
     })
-      .then((response) => response.json())
-      .then((result) => {
+      .then(async (response) => response.json())
+      .then(async (result) => {
         console.log("Success:", result);
+        const res = await fetch(
+          `${BASE_URL}/api/v1/status/file/${result.task_id},`,
+          { method: "PATCH" }
+        );
+        setIsFileSent(res);
       })
       .catch((error) => {
         console.error("Error:", error);
@@ -32,7 +40,12 @@ export const FileUpload = () => {
 
   return (
     <div>
-      <input type="file" name="file" onChange={changeHandler} />
+      <input
+        className="border border-gray-300 rounded-md px-4 py-2 w-full"
+        type="file"
+        name="file"
+        onChange={changeHandler}
+      />
       {isFilePicked ? (
         <div>
           <p>Filename: {selectedFile.name}</p>
@@ -47,7 +60,10 @@ export const FileUpload = () => {
         <p>Select a file to show details</p>
       )}
       <div>
-        <button onClick={handleSubmission}>Submit</button>
+        <Button variant="contained" onClick={handleSubmission}>
+          Upload File
+        </Button>
+        {isFileSent && <CircularIndeterminate />}
       </div>
     </div>
   );
